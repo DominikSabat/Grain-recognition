58b222430eb30e60517c41ef9d6403317abe0085
DominikSabat <60387429+DominikSabat@users.noreply.github.com>
2021-03-31 17:02:42 +0200

    Add files via upload

---
+++ src/Rectangle.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+
+public class Rectangle {
+
+    Point origin;
+    Color kolor;
+
+    int width;
+    int height;
+
+    public Rectangle(Point origin, int width, int height){
+        this.height = height;
+        this.width = width;
+        this.origin = origin;
+    }
+}
